d2e662f398c0c24c72e1b31be63034d8e023c66e
oys2021 <opokuyawsarfo3@gmail.com>
2025-04-23 10:09:38 +0000

    We do not dorge bro

---
+++ src/Userservice.java
@@ -0,0 +1,9 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class Userservice {
+    Map<Integer,String>  userMap;
+    public Userservice(){
+        this.userMap = new HashMap<>();
+    }
+}
